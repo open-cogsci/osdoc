8f44f7c4f9a798c26e07a07f41167c173e254b95
smathot <s.mathot@cogsci.nl>
2023-04-25 19:53:58 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -3,9 +3,10 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "Building documentation for $BRANCH"
 rm -Rf output/$BRANCH
 mkdir output/$BRANCH
-cp -R static/* output/$BRANCH
 python build-menu.py --publish
+cp -R static/* output/$BRANCH
 pelican -s publishconf.py
 cp output/$BRANCH/index/index.html output/$BRANCH/index.html
 cp output/$BRANCH/fr/index/index.html output/$BRANCH/fr/index.html
+cp output/$BRANCH/zh/index/index.html output/$BRANCH/zh/index.html
 python parse-theme.py
