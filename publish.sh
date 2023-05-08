72c48078897f8222301d3d30849505ca4a4120d0
smathot <s.mathot@cogsci.nl>
2023-05-08 12:05:09 +0200

    Update publish script for Spanish

--- publish.sh
+++ publish.sh
@@ -10,4 +10,5 @@ cp output/$BRANCH/index/index.html output/$BRANCH/index.html
 cp output/$BRANCH/fr/index/index.html output/$BRANCH/fr/index.html
 cp output/$BRANCH/zh/index/index.html output/$BRANCH/zh/index.html
 cp output/$BRANCH/de/index/index.html output/$BRANCH/de/index.html
+cp output/$BRANCH/es/index/index.html output/$BRANCH/es/index.html
 python parse-theme.py
